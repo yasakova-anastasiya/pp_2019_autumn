be5933b612e06dd9ecdc8c7f85644a7ff49edc8c
Vlad Nazarov <41645358+LonelyBaka@users.noreply.github.com>
2019-09-30 18:18:06 +0300

    Назаров Владислав. Задача 1. Сумма элементов матрицы. (#27)
    * Add task 1
    
    * Fixed lint errors
    
    * time fixed

---
+++ modules/task_1/nazarov_v_matrix_sum/matrix_sum.h
@@ -0,0 +1,11 @@
+// Copyright 2019 Nazarov Vladislav
+#ifndef MODULES_TASK_1_NAZAROV_V_MATRIX_SUM_MATRIX_SUM_H_
+#define MODULES_TASK_1_NAZAROV_V_MATRIX_SUM_MATRIX_SUM_H_
+
+#include <vector>
+
+std::vector<int> getRandomMatrix(int n, int m);
+int getParallelSum(std::vector<int> matrix, int matrix_size_n, int matrix_size_m);
+int getSequentialSum(std::vector<int> vec);
+
+#endif  // MODULES_TASK_1_NAZAROV_V_MATRIX_SUM_MATRIX_SUM_H_
