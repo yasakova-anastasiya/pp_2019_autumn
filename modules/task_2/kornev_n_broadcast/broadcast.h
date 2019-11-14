7fbda08cb543d655ebe2eb5d87d81cc12bbe5e67
Nesterov Alexander <nesoldr@gmail.com>
2019-11-14 11:57:41 +0300

    Revert "Корнев Никита. Задача 2. Передача от одного всем.  (#147)" (#188)
    This reverts commit cf3b937e262f1b69790358eb4ce4c905a7fea1ef.

--- modules/task_2/kornev_n_broadcast/broadcast.h
+++
@@ -1,9 +0,0 @@
-#ifndef MODULES_TASK_2_KORNEV_N_BROADCAST_BROADCAST_H_
-#define MODULES_TASK_2_KORNEV_N_BROADCAST_BROADCAST_H_
-// Copyright 2019 Kornev Nikita
-
-#include "./mpi.h"
-
-void broadcast(void* buffer, int count, MPI_Datatype datatype, int root, MPI_Comm comm);
-
-#endif  // MODULES_TASK_2_KORNEV_N_BROADCAST_BROADCAST_H_
