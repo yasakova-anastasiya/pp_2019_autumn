aabbed682cb8860380454bcc06adca9d9de8acd9
Nesterov Alexander <nesoldr@gmail.com>
2019-11-16 10:04:47 +0300

    Revert "Назаров Владислав. Задача 2. Задача "Спящий брадобрей" (#193)" (#217)
    This reverts commit c6f0209d58c8f5f454d4568c291f681715fdd319.

--- scripts/run.sh
+++ scripts/run.sh
@@ -15,11 +15,8 @@ for file in $FILES_TBB; do
     ./$file --gtest_repeat=10
 done
 
-file_nazarov="build/bin/nazarov_v_sleeping_barber_mpi"
 FILES_MPI="build/bin/*_mpi"
 for file in $FILES_MPI; do
-    if [ "$file" != "$file_nazarov" ]
-    then 
     echo "--------------------------------"
     echo $(basename $file)
     echo "--------------------------------"
@@ -35,30 +32,4 @@ for file in $FILES_MPI; do
     for i in {1..10}; do
         mpirun -np $NUM_PROC $file || exit 1
     done
-    fi
 done
-
-echo "--------------------------------"
-echo $(basename $file_nazarov)
-echo "--------------------------------"
-if  [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-    NUM_PROC="3"
-    echo "NUM_PROC: " $NUM_PROC
-    for i in {1..10}; do
-        mpirun -np $NUM_PROC $file_nazarov
-    done
-elif [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    NUM_PROC="3"
-    echo "NUM_PROC: " $NUM_PROC
-    for i in {1..10}; do
-        export OMPI_MCA_btl=self,tcp
-        mpirun -np $NUM_PROC --oversubscribe $file_nazarov
-    done
-else
-    echo "Unknown OS"
-    NUM_PROC="3"
-    echo "NUM_PROC: " $NUM_PROC
-    for i in {1..10}; do
-        mpirun -np $NUM_PROC $file_nazarov
-    done
-fi
