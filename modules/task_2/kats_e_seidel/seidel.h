c779c929e58476fbc0c4cbe99f47d4f047e31bda
DrXlor <34024471+DrXlor@users.noreply.github.com>
2019-12-02 23:03:30 +0300

    Кац Евгений. Задача 2. Итеративные методы Зейделя. (#264)
    * Task 1 is done
    
    * Task 2 is done
    
    * removed task_1
    
    * Test logic reworked
    
    * Minor changes
    
    * Minor changes

---
+++ modules/task_2/kats_e_seidel/seidel.h
@@ -0,0 +1,18 @@
+// Copyright 2019 Kats Eugeniy
+
+#ifndef MODULES_TASK_2_KATS_E_SEIDEL_SEIDEL_H_
+#define MODULES_TASK_2_KATS_E_SEIDEL_SEIDEL_H_
+
+#include <vector>
+
+#define TYPE_A 1
+#define TYPE_B 2
+
+std::vector<double> randMatrix(int n, int type);
+bool norm(std::vector<double> x, std::vector<double> p, double eps);
+std::vector<double> seidel_solve(std::vector<double> A, std::vector<double> B,
+                                 int n, double eps);
+std::vector<double> seidel_solve_s(std::vector<double> A, std::vector<double> B,
+                                   int n, double eps);
+
+#endif  // MODULES_TASK_2_KATS_E_SEIDEL_SEIDEL_H_
