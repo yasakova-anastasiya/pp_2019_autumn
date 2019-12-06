09318864ca2aba049cdb56881474e4f5f7f38838
Сергей Коннов <43045869+SergeyKonnov@users.noreply.github.com>
2019-12-06 19:21:22 +0300

    Коннов Сергей. Задача 2. Ленточная горизонтальная схема - разбиение только матрицы А fix (#275)
    * fix parallel multiplication
    
    * fix cpplint
    
    * add gatherv and transpose b

--- modules/task_2/konnov_s_matrix_mult_only_hor_a/matrix_mult_only_hor_a.cpp
+++ modules/task_2/konnov_s_matrix_mult_only_hor_a/matrix_mult_only_hor_a.cpp
@@ -4,56 +4,66 @@
 #include <iostream>
 #include <random>
 #include <vector>
+#include <utility>
 
 std::vector<int> matrix_mult_parallel(const std::vector<int>& aa, const std::vector<int>&bb, int msize) {
-    std::vector<int> a = aa, b = bb;
+    std::vector<int> a, b = bb;
     std::vector<int> ans;
     int rank, size;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
     b.resize(msize * msize);
+    if (rank == 0) {
+        for (int i = 0; i < msize; i++)
+            for (int j = 0; j < i; j++)
+                std::swap(b[i*msize+j], b[j*msize+i]);
+    }
     MPI_Bcast(b.data(), msize * msize, MPI_INT, 0, MPI_COMM_WORLD);
+
     int portion = msize * ((rank < msize % size)?msize / size + 1:msize / size);
-    if (rank != 0)
-        a.resize(portion*size);
+    a.resize(portion + 1);
+
     if (rank == 0) {
         int teq = portion;
         for (int i = 1; i < size; i++) {
             if (teq == msize*msize)
                 break;
             int teqportion = msize*((i < msize % size)?msize / size + 1:msize / size);
-            MPI_Send(a.data()+teq, teqportion, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(aa.data()+teq, teqportion, MPI_INT, i, 0, MPI_COMM_WORLD);
             teq+=teqportion;
+        }
+        for (int i = 0; i < portion; i++) {
+            a[i] = aa[i];
         }
     } else if (portion > 0) {
         MPI_Status status;
         MPI_Recv(a.data(), portion, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
     }
-    if (rank == 0) {
-        ans.resize(msize * msize);
-    } else {
-        ans.resize(msize * portion);
-    }
+
+    ans.resize(portion);
     for (int i = 0; i < portion / msize; i++)
         for (int j = 0; j < msize; j++) {
              ans[i * msize + j] = 0;
              for (int k = 0; k < msize; k++)
-                 ans[i * msize + j] += a[i * msize + k] * b[k * msize + j];
-          }
-    if (rank == 0) {
-        int teq = portion;
-        for (int i = 1; i < size; i++) {
-            if (teq == msize * msize)
-                break;
-            int teqportion = msize * ((i < msize % size)?msize / size + 1:msize / size);
-            MPI_Status status;
-            MPI_Recv(ans.data() + teq, teqportion, MPI_INT, i, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-            teq+=teqportion;
+                 ans[i * msize + j] += a[i * msize + k] * b[j * msize + k];
         }
-    } else if (portion > 0) {
-        MPI_Send(&ans[0], portion, MPI_INT, 0, 0, MPI_COMM_WORLD);
+
+    std::vector<int> ans2;
+    if (rank == 0)
+        ans2.resize(msize*msize);
+    std::vector<int> recvcounts(size), displs(size);
+    for (int i = 0 ; i < size; i++) {
+        recvcounts[i] =  msize * ((i < msize % size)?msize / size + 1:msize / size);
+        if (i != 0) {
+            displs[i] = displs[i-1] + recvcounts[i-1];
+        } else {
+            displs[i] = 0;
+        }
     }
-    return ans;
+    MPI_Gatherv(ans.data(), portion, MPI_INT,
+                ans2.data(), recvcounts.data(), displs.data(), MPI_INT, 0, MPI_COMM_WORLD);
+    return ans2;
 }
 
 std::vector<int> matrix_mult_sequential(const std::vector<int>& a, const std::vector<int>&b, int msize) {
